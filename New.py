2ce241db0b49374b33255fb5523c23b19a2351c0
Jobk2006 <jobmon7@gmail.com>
2024-11-22 21:04:37 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,1 +1,9 @@
 hii
+#author job 
+num=int(input("enter your number")
+        if(num%2==0):
+        print (f"{num} is an even number")
+        else:
+        print(f"{num} is a odd number")
+        
+        
