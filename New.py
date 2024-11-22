805d2fe5919881604d532ef2cfd0a127ebe16126
jesmet <jesmetsunny9@gmail.com>
2024-11-22 21:10:16 +0530

    Update New.py

--- New.py
+++ New.py
@@ -5,5 +5,15 @@ num=int(input("enter your number")
         print (f"{num} is an even number")
         else:
         print(f"{num} is a odd number")
-        
-        
+
+temp=int(input("enter the temperature "))
+If temp<0:
+    Print('stay indoor and drink hot chocolate ')
+elif temp>0 and temp<10:
+       Print('wear warm clothes and go for a walk')
+elif temp>10 and temp<20:
+       Print('perfect weather for a picnic')
+elif temp>21 and temp<30:
+      Print('great day for a beach')
+else:
+       Print('stay cool and hydrated indoors')
